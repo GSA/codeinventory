b88530da14d13ab150bae36d21c842ababa178ea
Jeff Fredrickson <jeffrey.fredrickson@gsa.gov>
2016-11-09 15:32:43 -0500

    add fixture support

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,14 @@
-$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-require 'codeinventory'
-require 'minitest/spec'
-require 'minitest/autorun'
+$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
+require "codeinventory"
+require "minitest/spec"
+require "minitest/autorun"
+require "pathname"
+
+def file_fixture(fixture_name)
+  file = Pathname.new(File.dirname(__FILE__)) + "fixtures" + fixture_name
+  if File.exists? file
+    file
+  else
+    raise ArgumentError, "the fixtures directory does not contain a file named '#{fixture_name}'"
+  end
+end
