c451916ec97f09ddba00d549266ec1b59209986d
Jeff Fredrickson <jeffrey.fredrickson@gsa.gov>
2016-11-10 16:39:20 -0500

    add spec for projects method

--- spec/source/json_file_spec.rb
+++ spec/source/json_file_spec.rb
@@ -13,4 +13,16 @@ describe "CodeInventory::Source::JSONFile" do
       end
     end
   end
+
+  describe ".projects" do
+    before do
+      json_file = file_fixture("source_json_two_projects_valid.json")
+      @json_source = CodeInventory::Source::JSONFile.new(json_file)
+    end
+
+    it "provides a list of projects" do
+      @json_source.projects.count.must_equal 2
+      @json_source.projects.first["name"].must_equal "Product One"
+    end
+  end
 end
