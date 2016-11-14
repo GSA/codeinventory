3490724347335a3f64fded11eaaf18c2e2ac602f
Jeff Fredrickson <jeffrey.fredrickson@gsa.gov>
2016-11-14 11:19:41 -0500

    add webmock

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,10 @@ $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "codeinventory"
 require "minitest/spec"
 require "minitest/autorun"
+require "webmock/minitest"
 require "pathname"
+
+WebMock.disable_net_connect!(allow_localhost: true)
 
 def file_fixture(fixture_name)
   file = Pathname.new(File.dirname(__FILE__)) + "fixtures" + fixture_name
