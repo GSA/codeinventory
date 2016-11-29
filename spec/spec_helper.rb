15a05d355e42d52bead93996284a4d23e2982adf
Jeff Fredrickson <jeffrey.fredrickson@gsa.gov>
2016-11-29 13:43:35 -0800

    resolve circular dependency

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,5 @@
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "codeinventory"
-require "minitest/spec"
 require "minitest/autorun"
 require "webmock/minitest"
 require "pathname"
