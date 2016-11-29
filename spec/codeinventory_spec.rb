1c27fc6382a97d440ad404ba782efb8546d0fc1d
Jeff Fredrickson <jeffrey.fredrickson@gsa.gov>
2016-11-29 13:37:03 -0800

    add parens to avoid ambiguous argument warning

--- spec/codeinventory_spec.rb
+++ spec/codeinventory_spec.rb
@@ -2,6 +2,6 @@ require 'spec_helper'
 
 describe 'CodeInventory' do
   it 'has a semantic version number' do
-    CodeInventory::VERSION.must_match /^([0-9]+)\.([0-9]+)\.([0-9]+)(?:-([0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*))?(?:\+[0-9A-Za-z-]+)?$/
+    CodeInventory::VERSION.must_match(/^([0-9]+)\.([0-9]+)\.([0-9]+)(?:-([0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*))?(?:\+[0-9A-Za-z-]+)?$/)
   end
 end
